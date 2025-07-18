9c02d07f12514c6ccb912b2b51911b0d1ff1d324
Jeremy Yu <102616407+jeremyyuAWS@users.noreply.github.com>
2025-07-18 09:17:01 -0700

    Fix MIME type and logo loading errors

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,30 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript'
+    }
+  },
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.');
+          const ext = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+            return `images/[name]-[hash][extname]`;
+          }
+          if (/css/i.test(ext)) {
+            return `css/[name]-[hash][extname]`;
+          }
+          return `assets/[name]-[hash][extname]`;
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
